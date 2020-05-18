9f4bc7d2aff7b2081d3638d58f434823537092a2
josemagb23 <jmgallardob98@gmail.com>
2020-05-18 20:49:43 +0200

    Cliente tarea 2 envio datos presidente

--- Tarea 2/Cliente/MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
+++ Tarea 2/Cliente/MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -4,11 +4,25 @@ import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.res.Resources;
 import android.os.Bundle;
+import android.os.StrictMode;
+import android.util.Base64;
 import android.view.View;
 import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.NoSuchAlgorithmException;
+import java.security.PrivateKey;
+import java.security.Signature;
+
+import javax.net.SocketFactory;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -17,10 +31,26 @@ public class MainActivity extends AppCompatActivity {
     protected static String server = "192.168.1.133";
     protected static int port = 7070;
 
+    String idUsuario;
+    EditText idUsuarioInput;
+
+    String idVotacion;
+    EditText idVotacionInput;
+
+    public static KeyPair getRSAKeyPair() throws NoSuchAlgorithmException {
+        KeyPairGenerator kpg = KeyPairGenerator.getInstance("RSA");
+        kpg.initialize(2048);
+        KeyPair kp = kpg.generateKeyPair();
+        return kp;
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        idUsuario = (EditText) findViewById(R.id.idUsuarioInput);
+        idVotacion = (EditText) findViewById(R.id.idVotacionInput);
 
         // Capturamos el boton de Enviar
         View button = findViewById(R.id.button_send);
@@ -32,11 +62,9 @@ public class MainActivity extends AppCompatActivity {
                 showDialog();
             }
         });
-
-
     }
 
-    // Creación de un cuadro de dialogo para confirmar pedido
+    // Creación de un cuadro de dialogo para confirmar datos
     private void showDialog() throws Resources.NotFoundException {
         CheckBox sabanas = (CheckBox) findViewById(R.id.checkBox_sabanas);
 
@@ -52,14 +80,64 @@ public class MainActivity extends AppCompatActivity {
 
                                 // Catch ok button and send information
                                 public void onClick(DialogInterface dialog, int whichButton) {
+                                    if (android.os.Build.VERSION.SDK_INT > 9)
+                                    {
+                                        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+                                        StrictMode.setThreadPolicy(policy);
+                                    }
 
                                     // 1. Extraer los datos de la vista
 
+                                    idUsuario = idUsuarioInput.getText().toString();
+                                    idVotacion = idVotacionInput.getText().toString();
+
+                                    String mensaje = idUsuario+", "+idVotacion;
+
                                     // 2. Firmar los datos
+
+                                    try{
+
+                                        KeyPair kp = getRSAKeyPair();
+                                        PrivateKey privateKey = kp.getPrivate();
+
+
+                                        Signature sg = Signature.getInstance("SHA256withRSA");
+                                        sg.initSign(privateKey);
+                                        sg.update(mensaje.getBytes());
+                                        byte[] firma = sg.sign();
+
+                                        String firmaMensaje = Base64.encodeToString(firma, Base64.DEFAULT);
+
+                                        String mensajeenviar = mensaje+"/"+firmaMensaje;
+                                    }
+
+                                    catch(Exception e){
+                                        e.printStackTrace();
+                                    }
 
                                     // 3. Enviar los datos
 
-                                    Toast.makeText(MainActivity.this, "Petición enviada correctamente", Toast.LENGTH_SHORT).show();
+                                    try {
+
+                                        SocketFactory socketFactory = (SocketFactory) SocketFactory.getDefault();
+
+                                        Socket socket = (Socket) socketFactory.createSocket("192.168.1.134", 8088);
+
+                                        PrintWriter output = new PrintWriter(new OutputStreamWriter(
+                                                socket.getOutputStream()));
+
+                                        output.println(mensajeenviar);
+                                        output.flush();
+
+
+                                        output.close();
+                                        socket.close();
+
+                                    } catch (Exception ioException) {
+                                        ioException.printStackTrace();
+                                    }
+
+                                    Toast.makeText(MainActivity.this, "Datos enviados correctamente", Toast.LENGTH_SHORT).show();
                                 }
                             }
 
